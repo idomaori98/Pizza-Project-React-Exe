fc42002ff50b47e0677b30751fdce8b3ef6a3a7a
idomaori98 <idoma@mta.ac.il>
2026-01-04 16:17:42 +0200

    extracting JSX  into a new component

--- src/index.js
+++ src/index.js
@@ -122,13 +122,7 @@ function Footer() {
     return (
         <footer className="footer">
             {isOpen ? (
-                <div className="order">
-                    <p>
-                        We are open until {closeHour}:00. Come visit us or order
-                        online.
-                    </p>
-                    <button className="btn">Order</button>
-                </div>
+                <Order closeHour={closeHour} />
             ) : (
                 <p>
                     We're happy to welcome you between {closeHour}:00 and
@@ -136,6 +130,18 @@ function Footer() {
                 </p>
             )}
         </footer>
+    );
+}
+
+function Order(props) {
+    return (
+        <div className="order">
+            <p>
+                We are open until {props.closeHour}:00. Come visit us or order
+                online.
+            </p>
+            <button className="btn">Order</button>
+        </div>
     );
 }
 
